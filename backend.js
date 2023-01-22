2f7a8d5df1e0af0e944bfb3edaf9bfea535d629f
CanlinCao <63250510+CanlinCao@users.noreply.github.com>
2023-01-21 21:00:23 -0800

    Finally done with backend Assignment

--- backend.js
+++ backend.js
@@ -47,10 +47,26 @@ app.listen(port, () => {
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
+    const job = req.query.job;
+    console.log(name);
+    console.log(job);
     if (name != undefined){
+        console.log(name);
         let result = findUserByName(name);
         result = {users_list: result};
-        res.send(result);
+        console.log("Name Filter:");
+        console.log(result);
+
+        if(job != undefined){
+            console.log("Job Filter: ");
+            console.log(job);
+            let jobResult = findUserByJob(job);
+            result = {user: jobResult};
+            res.send(result);
+        }
+        else{
+            res.send(result);
+        }
     }
     else{
         res.send(users);
@@ -59,6 +75,10 @@ app.get('/users', (req, res) => {
 
 const findUserByName = (name) => { 
     return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+const findUserByJob = (job) => { 
+    return users['users_list'].filter( (user) => user['job'] === job); 
 }
 
 app.get('/users/:id', (req, res) => {
@@ -71,6 +91,31 @@ app.get('/users/:id', (req, res) => {
         res.send(result);
     }
 });
+// app.get('/users/:job', (req, res) => {
+//     const name = req.query.name;
+//     const job =req.params['job'];
+//     console.log(name);
+//     console.log(job);
+//     if (name != undefined){
+//         console.log(name);
+//         let result = findUserByName(name);
+//         result = {users_list: result};
+//         console.log(result);
+
+//         if(job != undefined){
+//             console.log(job);
+//             let jobResult = findUserByJob(job);
+//             result = {result: job};
+//             res.send(result);
+//         }
+//         else{
+//             res.send(result);
+//         }
+//     }
+//     else{
+//         res.send(users);
+//     }
+// });
 
 function findUserById(id) {
     return users['users_list'].find( (user) => user['id'] === id); // or line below
@@ -80,9 +125,32 @@ function findUserById(id) {
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
-    res.status(200).end();
+    res.status(201).end();
 });
 
 function addUser(user){
     users['users_list'].push(user);
+}
+
+
+app.delete('/users', (req, res) => {
+    const deleteID = req.body.id;
+    //console.log(deleteID);
+    deleteUser(deleteID);
+    res.status(200).end();
+});
+
+
+function findUserIndex(userID){
+    return users['users_list'].findIndex( (user) => user['id'] === userID);
+}
+
+function deleteUser(userID){
+    index = findUserIndex(userID);
+    if (index == -1){
+        console.log("User does not Exist");
+    }
+    else{
+        users['users_list'].splice(index,1);
+    }
 }
